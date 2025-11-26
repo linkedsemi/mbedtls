8e993f40d6f3657b8f8aee452ce5f4aa34f40572
houruiying <ryhou@linkedsemi.com>
2025-11-26 14:56:11 +0800

    sha384/sha512: Change blocking to non-blocking implementation

--- library/sha512_alt.c
+++ library/sha512_alt.c
@@ -37,22 +37,65 @@ void ls_otbn_mbedtls_update_callback(void (*func)(void*),void *param);
 
 #if defined(CONFIG_MBEDTLS_SHA384_SHA512_LINKEDSEMI_HARDWARE_ALT)
 #include <ls_hal_sha512.h>
+#include "field_manipulate.h"
+#include <zephyr/cache.h>
+#include "reg_sha512_type.h"
+#include <core_rv32.h>
+#include "qsh.h"
 static mbedtls_threading_mutex_t doneLock;
 static mbedtls_sha512_context* ls_sha_ctx = NULL;
+__attribute__((aligned(4))) static uint32_t buffer[0x20];
+struct k_sem sha384_sha512_sem;
+#define SHA384_SHA512_WAIT_TIMEOUT_MS 100000
+static uint32_t total_cnt;
+static uint32_t buffer_idx;
+static bool isFirst;
+static uint8_t read_reg_count;
+
+void LSSHA384_SHA512_IRQHandler(void)
+{
+    if(LS_SHA512->INTR_STT & SHA512_INTR_CALC_END_MASK)
+    {
+        LS_SHA512->INTR_CLR = LS_SHA512->INTR_STT;
+        LS_SHA512->INTR_MSK = 0x0;
+        k_sem_give(&sha384_sha512_sem);
+    }
+}
+
+static void block_calculate(uint32_t addr, uint32_t block_number)
+{
+    while ((LS_SHA512->STATUS & 0x1) != 0x1) ;
+    REG_FIELD_WR(LS_SHA512->CTRL, SHA512_CTRL_BLOCK_NUM, (block_number - 1));
+    LS_SHA512->ADDR = addr;
+    assert(((uint32_t)addr % 4) == 0);
+    csi_dcache_clean_range((uint32_t *)addr, block_number*LS_SHA512_BLOCK_SIZE);
+
+    LS_SHA512->INTR_MSK = SHA512_INTR_CALC_END_MASK;
+
+    if (isFirst)
+    {
+        REG_FIELD_WR(LS_SHA512->CTRL, SHA512_CTRL_INIT_CALC, 1);
+        REG_FIELD_WR(LS_SHA512->CTRL, SHA512_CTRL_START, 1);
+        isFirst = false;
+    }
+    else
+    {
+        REG_FIELD_WR(LS_SHA512->CTRL, SHA512_CTRL_INIT_CALC ,0);
+        REG_FIELD_WR(LS_SHA512->CTRL, SHA512_CTRL_START, 1);
+    }
+
+    k_sem_take(&sha384_sha512_sem, K_MSEC(SHA384_SHA512_WAIT_TIMEOUT_MS));
+}
 
 void mbedtls_sha512_init(mbedtls_sha512_context *ctx)
 {
     memset(ctx, 0, sizeof(mbedtls_sha512_context));
-    // mbedtls_zephyr_threading_init();
     mbedtls_mutex_init(&doneLock);
-    
-    #if CONFIG_SHA512_CLOCK_RESET
-    #include "reg_sysc_sec_cpu.h"
-        SYSC_SEC_CPU->PD_CPU_CLKG[0] = SYSC_SEC_CPU_CLKG_CLR_SHA512_MASK;
-        SYSC_SEC_CPU->PD_CPU_SRST[0] = SYSC_SEC_CPU_SRST_CLR_SHA512_MASK;
-        SYSC_SEC_CPU->PD_CPU_SRST[0] = SYSC_SEC_CPU_SRST_SET_SHA512_MASK;
-        SYSC_SEC_CPU->PD_CPU_CLKG[0] = SYSC_SEC_CPU_CLKG_SET_SHA512_MASK;
-    #endif
+    k_sem_init(&sha384_sha512_sem, 0, 1);
+    LS_SHA512->INTR_MSK = 0x0;
+    LS_SHA512->INTR_CLR = SHA512_INTR_DMA_END_MASK | SHA512_INTR_CALC_END_MASK;
+    IRQ_CONNECT(SHA512_IRQN, 3, LSSHA384_SHA512_IRQHandler,NULL, 0);
+    irq_enable(SHA512_IRQN);
 }
 
 void mbedtls_sha512_free(mbedtls_sha512_context *ctx)
@@ -83,10 +126,14 @@ int mbedtls_sha512_starts(mbedtls_sha512_context *ctx, int is384)
     if (is384) 
     {
         ctx->is384 = true;
-        HAL_SHA384_SHA384_Init();
+        isFirst = true;
+        read_reg_count = 12;
+        LS_SHA512->CTRL = 0x8;
     } else {
         ctx->is384 = false;
-        HAL_SHA512_SHA512_Init();
+        isFirst = true;
+        read_reg_count = 16;
+        LS_SHA512->CTRL = 0xc;
     }
 
     return 0;
@@ -103,8 +150,35 @@ int mbedtls_sha512_update(mbedtls_sha512_context *ctx,
         ctx->start_calc_symbol = true;
     }
     assert(ls_sha_ctx == ctx);
+    assert(((uint32_t)input % 4) == 0);
+    uint8_t *msg = (uint8_t *)input;
+    total_cnt += ilen;
 
-    HAL_SHA512_SHA512_Update((uint32_t *)input, ilen);
+    if (buffer_idx)
+    {
+        if ((ilen + buffer_idx) < LS_SHA512_BLOCK_SIZE)
+        {
+            memcpy(&buffer[buffer_idx], input, ilen);
+            buffer_idx += ilen;
+            return 0;
+        }
+        uint32_t wr_len = LS_SHA512_BLOCK_SIZE - buffer_idx;
+        memcpy(&buffer[buffer_idx], msg, wr_len);
+        block_calculate((uint32_t)buffer, 1);
+        buffer_idx = 0;
+        ilen -= wr_len;
+        msg += wr_len;
+    }
+
+    uint32_t block_number = ilen / LS_SHA512_BLOCK_SIZE;
+    if (block_number)
+        block_calculate((uint32_t)msg, block_number);
+
+    if (ilen % LS_SHA512_BLOCK_SIZE)
+    {
+        memcpy(&buffer[buffer_idx], msg + (block_number * LS_SHA512_BLOCK_SIZE), ilen % LS_SHA512_BLOCK_SIZE);
+        buffer_idx = ilen % LS_SHA512_BLOCK_SIZE;
+    }
     return 0;
 }
 
@@ -112,8 +186,43 @@ int mbedtls_sha512_finish(mbedtls_sha512_context *ctx,
                           unsigned char *output)
 {
     assert(ls_sha_ctx == ctx);
-    HAL_SHA512_SHA512_Final(output);
+    uint8_t *p_buffer = (uint8_t *)buffer;
+    uint64_t bit_cnt = total_cnt * 8;
+    p_buffer[buffer_idx++] = 0x80;
+    if (buffer_idx == LS_SHA512_BLOCK_SIZE)
+    {
+        block_calculate((uint32_t)buffer, 1);
+        buffer_idx = 0;
+    }
 
+    while (buffer_idx != (LS_SHA512_BLOCK_SIZE - 0x10))
+    {
+        p_buffer[buffer_idx++] = 0x0;
+        if (buffer_idx == LS_SHA512_BLOCK_SIZE)
+        {
+            block_calculate((uint32_t)buffer, 1);
+            buffer_idx = 0;
+        }
+    }
+    memset(&p_buffer[buffer_idx], 0x0, 8);
+    buffer_idx += 8;
+
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        p_buffer[buffer_idx + (7 - i)] = (uint8_t)(bit_cnt >> (8 * i));
+    }
+    buffer_idx += 8;
+    block_calculate((uint32_t)buffer, 1);
+    for (uint8_t j = 0; j < read_reg_count; j++)
+    {
+        uint32_t in = LS_SHA512->DIGEST[15 - j];
+        *output++ = in >> 24;
+        *output++ = in >> 16;
+        *output++ = in >> 8;
+        *output++ = in;
+    }
+    buffer_idx = 0;
+    total_cnt = 0;
     ls_sha_ctx = NULL;
     ctx->start_calc_symbol = false;
     mbedtls_mutex_unlock(&doneLock);
